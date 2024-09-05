3e600e1c0692a32e04ee6d364cd4e29592dd810c
Zohre-M <zohre.arch@gmail.com>
2024-09-05 11:44:32 -0700

    added the current weather section

--- src/Weather.js
+++ src/Weather.js
@@ -21,10 +21,17 @@ export default function Weather() {
       </header>
       <main>
         <div className="current-weather">
-          <div>
+          <div className="current-weather-data">
             <h1>{city}</h1>
             <p>Monday 22:32, clear sky</p>
-            <p>Humidity: 89%, Wind: 1km/h</p>
+            <p>
+              Humidity: <span>89%</span>, Wind: <span>1km/h</span>
+            </p>
+          </div>
+          <div className="current-temperature">
+            <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png" />
+            <span className="current-temperature-amount">20</span>
+            <span className="unit">°C</span>
           </div>
         </div>
       </main>
